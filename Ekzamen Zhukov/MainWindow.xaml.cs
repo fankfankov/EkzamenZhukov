195c1731ee1eb3a2f6205ab68e1c9cd6b9fe2bf5
AnastasiaZvereva5 <zipz.ee@mail.ru>
2023-05-13 10:48:00 +0300

    AutorizForm

---
+++ Ekzamen Zhukov/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ekzamen.Model;
+
+namespace Ekzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txbpass.IsEnabled = false;
+        }
+
+        public static class Globals
+        {
+            public static int UserRoles;
+            public static user userinfo { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonNext1(object sender, RoutedEventArgs e)
+        {
+            var curentuser = AppData.db.user.FirstOrDefault(u => u.login == txblogin.Text);
+            if(curentuser != null)
+            {
+                Globals.userinfo = curentuser;
+                Globals.UserRoles = curentuser.roleID;
+                txbpass.IsEnabled = true;
+                button1.Visibility = Visibility.Hidden;
+                button2.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("Login doesnt exist!");
+            }
+        }
+
+        private void ButtonNext2(object sender, RoutedEventArgs e)
+        {
+            var curentuser = AppData.db.user.FirstOrDefault(u => u.login == txblogin.Text && u.pass == txbpass.Text);
+            if (curentuser != null)
+            {
+                Window1 window1 = new Window1();
+                window1.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Password incorrect!");
+            }
+        }
+    }
+}
---
+++ Ekzamen Zhukov/Window1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ekzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            if (MainWindow.Globals.UserRoles == 1)
+            {
+                txbroles.Text = "Заказчик";
+            }
+            if (MainWindow.Globals.UserRoles == 2)
+            {
+                txbroles.Text = "Менеджер";
+            }
+
+            if (MainWindow.Globals.UserRoles == 3)
+            {
+                txbroles.Text = "Кладовщик";
+            }
+            if (MainWindow.Globals.UserRoles == 4)
+            {
+                txbroles.Text = "Дирекция";
+            }
+        }
+
+        private void button2_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
